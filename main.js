22112727dd0f52531f75987d7e20bd851b626478
yogik23 <61941105+yogik23@users.noreply.github.com>
2025-05-11 14:49:38 +0700

    Update main.js

--- main.js
+++ main.js
@@ -3,12 +3,30 @@ const { arbmain } = require('./arb');
 const { plumemain } = require('./plume');
 const { pointmain } = require('./cekpoint');
 const { displayskw } = require('./skw/displayskw');
+const cron = require('node-cron');
+const chalk = require('chalk');
 
-(async () => {
+async function startBot() {
   console.clear();
   displayskw();
   await sepoliamain();
   await arbmain();
   await plumemain();
   await pointmain();
-})();
+}
+
+async function main() {
+  cron.schedule('0 1 * * *', async () => { 
+    await startBot();
+    console.log();
+    console.log(chalk.hex('#FF00FF')(`Cron AKTIF`));
+    console.log(chalk.hex('#FF1493')('Jam 08:00 WIB Autobot Akan Run'));
+  });
+
+  await startBot();
+  console.log();
+  console.log(chalk.hex('#FF00FF')(`Cron AKTIF`));
+  console.log(chalk.hex('#FF1493')('Jam 08:00 WIB Autobot Akan Run'));
+}
+
+main();
